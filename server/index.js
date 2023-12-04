027ffb950a11ea885838cb3d9e79b11f3bdd2a9a
vignesh <donapativigneshwar@gmail.com>
2023-12-04 22:28:21 +0530

    added static routes in backend

--- server/index.js
+++ server/index.js
@@ -10,6 +10,12 @@ const postRoute = require('./Routes/postRoute')
 
 const PORT = process.env.PORT;
 
+app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
